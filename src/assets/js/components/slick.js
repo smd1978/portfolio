0c156da88beec7f58beb91f85ee41144929e542e
Mihail <smd78@list.ru>
2022-07-02 18:41:27 +0500

    add_slick_slider_v_1

--- src/assets/js/components/slick.js
+++ src/assets/js/components/slick.js
@@ -1,1 +1,28 @@
-console.log(12);
+$(".portfolio").slick({
+    arrows: false, // не показывать стрелки
+    dots: true, // показывать точки индикаторы
+    dotsClass: 'slick-dots', // название класса для точек
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay:true, // автоматическое проигрывание слайдов
+
+
+
+
+//Добавляем адаптив
+
+responsive: [{
+    breakpoint: 1025, // максимальная ширина экрана
+    settings: {
+        slidesToShow: 2, // показывать по 2 слайда
+        slidesToScroll: 2, // скроллить по 2 слайда
+    }
+}, {
+    breakpoint: 688,
+    settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
+}]
+});      
