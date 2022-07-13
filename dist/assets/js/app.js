5f39bcd064726000f4764104cb52076912a16d73
Mihail <smd78@list.ru>
2022-07-13 13:19:09 +0500

    fix_stoic

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3010,33 +3010,15 @@
 
 }));
 
-$(function(){
-    $(".portfolio").slick({
-        arrows: false, // не показывать стрелки
-        dots: true, // показывать точки индикаторы
-        dotsClass: 'slick-dots', // название класса для точек
-        speed: 800,
-        slidesToShow: 3,
-        slidesToScroll: 2,
-        autoplay:true, // автоматическое проигрывание слайдов
-    
-    
-    
-    
-    //Добавляем адаптив
-    
-    responsive: [{
-        breakpoint: 1025, // максимальная ширина экрана
-        settings: {
-            slidesToShow: 2, // показывать по 2 слайда
-            slidesToScroll: 2, // скроллить по 2 слайда
-        }
-    }, {
-        breakpoint: 688,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-        }
-    }]
+$(function() {
+    $('.title__link').on('click', function(e) {
+    e.preventDefault();
+    $('.link__article').each(function() {
+    $(this).css('display', 'none');
     });
-});
+    var block = $(this).attr('href');
+    $(block).css('display', 'block');
+    });
+    });
+
+$('.').addClass('summary');
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,14 @@
 //= ../../../node_modules/slick-carousel/slick/slick.js
 
-$(function(){
-    //= components/slick.js
-});
+$(function() {
+    $('.title__link').on('click', function(e) {
+    e.preventDefault();
+    $('.link__article').each(function() {
+    $(this).css('display', 'none');
+    });
+    var block = $(this).attr('href');
+    $(block).css('display', 'block');
+    });
+    });
+
+$('.').addClass('summary');
